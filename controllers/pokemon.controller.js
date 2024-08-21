4c732a4839d9c9c51a30cee1a9f44aad08834979
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-08-21 16:06:22 +0200

    connect to mongodb atlas

--- controllers/pokemon.controller.js
+++ controllers/pokemon.controller.js
@@ -1,74 +1,25 @@
-
-
-let pokemon = [
-    { id: 1, name: "Pikachu", type: "Electric", owner: "Ash" },
-    { id: 2, name: "Charmander", type: "Fire", owner: "Ash" },
-    { id: 3, name: "Bulbasaur", type: "Grass/Poison", owner: "Ash" },
-    { id: 4, name: "Squirtle", type: "Water", owner: "Ash" },
-    { id: 5, name: "Jigglypuff", type: "Normal/Fairy", owner: "Jiggly" },
-    { id: 6, name: "Meowth", type: "Normal", owner: "Team Rocket" },
-    { id: 7, name: "Psyduck", type: "Water", owner: "Mist" },
-    { id: 8, name: "Machop", type: "Fighting", owner: "Brock" },
-    { id: 9, name: "Eevee", type: "Normal", owner: "Sylvia" },
-    { id: 10, name: "Gengar", type: "Ghost/Poison", owner: "Haunter" },
-    { id: 11, name: "Onix", type: "Rock/Ground", owner: "Brock" },
-    { id: 12, name: "Lapras", type: "Water/Ice", owner: "Lorelei" },
-    { id: 13, name: "Snorlax", type: "Normal", owner: "Nurse Joy" },
-    { id: 14, name: "Alakazam", type: "Psychic", owner: "Kadabra" },
-    { id: 15, name: "Gyarados", type: "Water/Flying", owner: "Red" }
-];
+const repository = require('../database/mongo.database');
 
 exports.getAllPokemon = (req, res) => {
-    res.send(pokemon);
+    
 }
 
 exports.getByID = (req, res) => {
-    const id  = parseInt(req.params.id);
-    const poke = pokemon.find(
-        (p) => p.id === id
-    );
-
-    if(!poke){
-        res.status(404).send();
-    }else{
-        res.send(poke);
-    }
+    
 }
 
 exports.updateByID = (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = pokemon.findIndex((p) => p.id === id);
-    console.log(id);
-
-    if (index > -1) {
-        const updatedData = req.body;
-        
-        if (updatedData.name !== undefined) {
-            pokemon[index].name = updatedData.name;
-        }
-        if (updatedData.type !== undefined) {
-            pokemon[index].type = updatedData.type;
-        }
-        if (updatedData.owner !== undefined) {
-            pokemon[index].owner = updatedData.owner;
-        }
-        res.send(pokemon[index]);
-    } else {
-        res.status(404).send({ message: "Pokemon not found" });
-    }
+    
 }
 
 
-exports.deleteByID = (req, res) => {
+exports.deleteByID = async (req, res) => {
     const id = parseInt(req.params.id);
-    const index = pokemon.findIndex((p) => p.id === id);
-    console.log(id);
-
-    if(index > -1){
-        const poke = pokemon[index];
-        pokemon.splice(index, 1);
-        res.send(poke);
+    
+    const result = await repository.deletePokemonById(id);
+    if(result.length > 1){
+        res.send(result);
     }else{
-        res.status(404).send({ message: "Pokemon not found" });
+        res.status(404).send({message: `pokemon with id ${id} not found`});
     }
 }
---
+++ database/mongo.database.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_DB_CONNSTR);
+
+const pokemonSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    type: String,
+    owner: String,
+});
+
+const pokemonModel = mongoose.model('Pokemon', pokemonSchema);
+
+
+/**
+ * creates a mongoose pokemonModel object from the given pokemon object
+ * and then adds a new document to the pokemons collection in the db
+ * @param {Pokemon} pokemon 
+ */
+exports.createPokemon = (pokemon) => {
+    const poke = new pokemonModel({
+        id: pokemon.id,
+        name: pokemon.name,
+        type: pokemon.type,
+        owner: pokemon.owner,
+    });
+    
+    poke.save();
+}
+
+exports.deletePokemonById = async (id) => {
+    const poke = await pokemonModel.find({
+        id: id
+    }).exec();
+    console.log(poke);
+    return poke;
+}
